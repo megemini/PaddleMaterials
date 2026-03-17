b3aefae9235a1e3bcee6ff022be554aad2a1684c
megemini <megemini@outlook.com>
2026-03-17 18:22:56 +0800

    refactor: 移除未使用的导入 BinaryActivityDatasetFromSMILES

--- ppmat/datasets/__init__.py
+++ ppmat/datasets/__init__.py
@@ -48,7 +48,6 @@ from ppmat.datasets.oc20_s2ef_dataset import OC20S2EFDataset  # noqa
 from ppmat.datasets.qm9_dataset import QM9Dataset # noqa
 from ppmat.datasets.omol25_dataset import OMol25Dataset
 from ppmat.datasets.binary_activity_dataset import BinaryActivityDataset
-from ppmat.datasets.binary_activity_dataset import BinaryActivityDatasetFromSMILES
 from ppmat.datasets.split_mptrj_data import none_to_zero
 from ppmat.datasets.transform import build_transforms
 from ppmat.utils import logger
